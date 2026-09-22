170374590587f1c509f87802bc72a887ca05eeb5
TearsDev <nicolasluque_facundo@hotmail.com>
2026-09-22 19:48:37 -0300

    Add footer and navigation structure; create initial database, auth, and functions files

---
+++ public/layout/footer.php
@@ -0,0 +1,22 @@
+<footer class="footer">
+    <div class="footer-col">
+        <img src="/assets/images/Logo_Nav.png" alt="Avanza">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, voluptatum consequatur. Similique iste repellendus repudiandae maiores doloremque nisi perspiciatis repellat facere in ut. Quae repudiandae laboriosam fugiat ipsum sunt. Sunt?</p>
+    </div>
+    <div class="footer-col">
+        <h4>Navegacion</h4>
+        <ul class="footer-links">
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Como usar</a></li>
+            <li><a href="">Contacto</a></li>
+        </ul>
+    </div>
+    <div class="footer-col">
+        <h4>Privacidad</h4>
+        <h4>Hola</h4>
+        <ul>
+            <li>Privacidad</li>
+            <li>Términos</li>
+        </ul>
+    </div>
+</footer>
--- public/layout/nav.php
+++ public/layout/nav.php
@@ -1,6 +1,6 @@
 <div class="nav-container">
     <div class="nav-logo">
-        <img src="" alt="Avanza">
+        <img src="/assets/images/Logo_Nav.png" alt="Avanza">
     </div>
     <div class="nav-menu">
         <ul>
@@ -8,5 +8,9 @@
             <li><a href="#">Cómo usar</a></li>
             <li><a href="#">Contacto</a></li>
         </ul>
+    </div>
+    <div class="navbar-acciones">
+        <a href="/">Iniciar sesión</a>
+        <a href="/">Solicitar una demo</a>
     </div>
 </div>
