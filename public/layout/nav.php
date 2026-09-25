23cbeb2ff11b5912a482032e6d51db5032628281
TearsDev <nicolasluque_facundo@hotmail.com>
2026-09-25 10:06:09 -0300

    actualizacion de bug de rutas

--- public/layout/nav.php
+++ public/layout/nav.php
@@ -4,13 +4,13 @@
     </a>
     <div class="nav-links">
         <ul>
-            <li><a href="/" class="active">Inicio</a></li>
-            <li><a href="/como-usar">Cómo usar</a></li>
-            <li><a href="/contacto">Contacto</a></li>
+            <li><a href="index.php" class="active">Inicio</a></li>
+            <li><a href="como-usar.php">Cómo usar</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
         </ul>
     </div>
     <div class="nav-actions">
-        <a href="/contacto" class="btn btn-outline">Contacto</a>
-        <a href="/login.php" class="btn btn-primary">Iniciar sesión</a>
+        <a href="contacto.php" class="btn btn-outline">Contacto</a>
+        <a href="login.php" class="btn btn-primary">Iniciar sesión</a>
     </div>
 </nav>
